297d5a921b2b54051be9ea2190fffc9f2cc453e6
molly wang <wm_java@163.com>
2017-03-03 09:46:26 +0800

    修改convert语句

--- fmdm.core/src/main/resources/META-INF/com/founder/fmdm/dao/rule/RlmgModelDao/selectRules.sql
+++ fmdm.core/src/main/resources/META-INF/com/founder/fmdm/dao/rule/RlmgModelDao/selectRules.sql
@@ -1,9 +1,9 @@
 select distinct rr.rule_id,
        rr.rule_Name,
        rr.apply_by,
-       convert(rr.apply_time, getdate(), 120) as apply_Time,
+       convert(varchar(20), rr.apply_time, 120) as apply_Time,
        rr.permit_by,
-       convert(rr.permit_Time, getdate(), 120) as permit_Time,
+       convert(varchar(20), rr.apply_time, 120) as permit_Time,
        rr.status,
        rr.memo,
        rr.opinions
